67b765e85a4b2733656dbdb354d5e7e97e4abecf
Casper Smet <casper.smet@gmail.com>
2022-02-25 13:15:02 +0100

    Implemented util functions for loading resnet data

--- koala/data.py
+++ koala/data.py
@@ -1,7 +1,10 @@
+from functools import cache
+from itertools import repeat
 from pathlib import Path
 
-from matplotlib import pyplot as plt
+import numpy as np
 from torch import float as tfloat
+from torch import tensor
 from torch.functional import Tensor
 from torch.utils.data import Dataset
 
@@ -27,6 +30,12 @@ DEFAULT_TRAIN = DEFAULT_DATA / Path("val_img.txt")
 DEFAULT_VAL = DEFAULT_DATA / Path("train_img.txt")
 DEFAULT_IMG_DIR = DEFAULT_DATA / Path(
     "./imagenet-object-localization-challenge/imagenet_object_localization_patched2019.tar/imagenet_object_localization_patched2019/ILSVRC/Data/CLS-LOC/val/"
+)
+DEFAULT_RESNET_LABELS = DEFAULT_DATA / Path(
+    "./imagenet-object-localization-challenge/LOC_val_solution.csv"
+)
+DEFAULT_SYSNET_MAPPING = DEFAULT_DATA / Path(
+    "./imagenet-object-localization-challenge/LOC_synset_mapping.txt"
 )
 
 
@@ -76,11 +85,33 @@ class KoalaDataset(Dataset):
         self.img_names = img_names
         self.img_dir = img_dir
         self.ext_format = ext_format
-        self.transforms = Compose([SquarePad(), Resize(img_size), ConvertImageDtype(tfloat)])
+        self.transforms = Compose(
+            [SquarePad(), Resize(img_size), ConvertImageDtype(tfloat)]
+        )
         self.grayscale = Grayscale(3)
 
     def __len__(self) -> int:
         return len(self.img_names)
+
+    def get_img(self, idx: int) -> Tensor:
+        """Reads the image at index idx from file system into Tensor, transforms appropriately.
+
+        Sub function for easier subclassing.
+
+        Args:
+            idx (int): image index
+
+        Raises:
+            FileNotFoundError: if file at path does not exist, raise before trying to read with torch.
+
+        Returns:
+            Tensor: original image (padded)
+        """
+        img_path = self.img_dir / self.ext_format.format(self.img_names[idx])
+        if not img_path.exists():
+            raise FileNotFoundError(f"Input image at {img_path} does not exist.")
+        img = self.transforms(read_image(img_path.as_posix(), ImageReadMode.RGB))
+        return img
 
     def __getitem__(self, idx: int) -> tuple[Tensor, Tensor]:
         """Reads the image at index idx from file system into Tensor, transforms appropriately.
@@ -94,13 +125,65 @@ class KoalaDataset(Dataset):
         Returns:
             tuple[Tensor, Tensor]: grayscaled image, original (padded)
         """
-        img_path = self.img_dir / self.ext_format.format(self.img_names[idx])
-        if not img_path.exists():
-            raise FileNotFoundError(f"Input image at {img_path} does not exist.")
-        img_y = self.transforms(read_image(img_path.as_posix(), ImageReadMode.RGB))
+        img_y = self.get_img(idx)
         img_x = self.grayscale(img_y)
 
         return img_x, img_y
+
+
+def resnet_labels_to_tensor(
+    id_label_pair: tuple[str, str], label_map: dict = None, num_classes: int = 1000
+) -> tuple[any, Tensor]:
+    """Turns a id-labels pair into a id-tensor pair
+
+    Args:
+        id_label_pair (tuple[str, str]): id label pair
+        label_map (dict, optional): map of labels to integers. Defaults to load_resnet_synset(DEFAULT_SYSNET_MAPPING).
+        num_classes (int, optional): Number of classes. Defaults to 1000.
+
+    Returns:
+        tuple[any, Tensor]: id, tensor pair
+    """
+    _, labels = id_label_pair
+    pairs = labels.split()[::5]
+    if label_map is None:
+        label_map = load_resnet_synset(DEFAULT_SYSNET_MAPPING)
+
+    label_arr = np.zeros(num_classes)
+    label_arr[[label_map[k] for k in pairs]] = 1
+    return _, tensor(label_arr, dtype=tfloat)
+
+
+def load_resnet_labels(p: Path, skip_header: bool = True) -> dict[str, Tensor]:
+    """Loads imagenet labels and converts them to Inception-Resnet labels.
+
+    Args:
+        p (Path): Path to label file
+        skip_header (bool, optional): Skip the .csv header. Defaults to True.
+
+    Returns:
+        dict[str, Tensor]: Mapping of label id's to tensors
+    """
+    lines = load_image_names(p)[:-1]
+    if skip_header:
+        lines = lines[1:]
+    id_label_pairs = map(str.split, lines, repeat(","))
+    id_tensor_pairs = map(resnet_labels_to_tensor, id_label_pairs)
+    return dict(id_tensor_pairs)
+
+
+@cache
+def load_resnet_synset(p: Path) -> dict[str, int]:
+    """Loads the synset label mapping (id to integer)
+
+    Args:
+        p (Path): Path to synset file
+
+    Returns:
+        dict[str, int]: id, int
+    """
+    mappings = load_image_names(p)[:-1]
+    return {line.split()[0]: i for i, line in enumerate(mappings)}
 
 
 def load_image_names(p: Path) -> list[str]:
@@ -109,6 +192,9 @@ def load_image_names(p: Path) -> list[str]:
 
 
 def main():
+    labels = load_resnet_labels(DEFAULT_RESNET_LABELS)
+    label = labels["ILSVRC2012_val_00003605"]
+    print(label)
     train = load_image_names(DEFAULT_TRAIN)
     _ = load_image_names(DEFAULT_VAL)
     ds = KoalaDataset(train, DEFAULT_IMG_DIR, "{}.JPEG")
