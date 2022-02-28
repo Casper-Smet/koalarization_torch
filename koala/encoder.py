de5e6ef55c5f0724cbd0fdfe5ac2e16eeaf3ce86
Casper Smet <casper.smet@gmail.com>
2022-02-28 16:36:25 +0100

    Lined up columns

--- koala/encoder.py
+++ koala/encoder.py
@@ -10,7 +10,7 @@ class Encoder(nn.Module):
     All layers use the ReLU activation function.
 
     layer   Kernels         Stride
-    conv    64 × (3 × 3)    2 × 2
+    conv    64  × (3 × 3)   2 × 2
     conv    128 × (3 × 3)   1 × 1
     conv    128 × (3 × 3)   2 × 2
     conv    256 × (3 × 3)   1 × 1
