1339459a07ad40be5275f80a7db0fa0bfcfe056c
Casper Smet <casper.smet@gmail.com>
2022-03-11 18:26:14 +0100

    Extended trainscript with cmd args

--- koala/train.py
+++ koala/train.py
@@ -1,7 +1,10 @@
+import argparse
+from pathlib import Path
+
 import torch
 from matplotlib import pyplot as plt
 from torch import nn, optim
-from torch.cuda import get_device_properties, is_available
+from torch.cuda import get_device_properties
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm, trange
@@ -15,15 +18,16 @@ from .data import (
 )
 from .network import Colorization
 
-BATCH_SIZE = 8
-LEARNING_RATE = 0.001
-DEVICE = "cuda" if is_available() else "cpu"
+DEFAULT_FORMAT = "{}.JPEG"
 EPOCHS = 5
+BATCH_SIZE = 8
 NUM_WORKERS = 6
-PIN_MEMORY = True
 DROP_LAST = True
+LEARNING_RATE = 0.001
 SHUFFLE = True
+PIN_MEMORY = True
 PERSISTENT_WORKERS = True
+DEVICE = "cuda"
 
 
 def norm(l, ab):
@@ -44,31 +48,90 @@ def plot(l, pab, tab):
     return fig
 
 
-def main():
+def parse_args() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description="Trains a PyTorch Koalarization network."
+    )
+
+    parser.add_argument(
+        "--img_dir",
+        type=Path,
+        default=DEFAULT_IMG_DIR,
+        help="Directory containing all images used.",
+    )
+    parser.add_argument(
+        "--train_filenames",
+        type=Path,
+        default=DEFAULT_TRAIN,
+        help="Path to image names used in training.",
+    )
+    parser.add_argument(
+        "--file_format",
+        type=str,
+        default=DEFAULT_FORMAT,
+        help='Format for images (e.g, "{}.jpeg".',
+    )
+    parser.add_argument("--epochs", type=int, default=EPOCHS, help="Amount of epochs.")
+    parser.add_argument(
+        "--learning_rate", type=float, default=LEARNING_RATE, help="Learning rate."
+    )
+    parser.add_argument(
+        "--batch_size", type=int, default=BATCH_SIZE, help="Amount of images per batch."
+    )
+    parser.add_argument(
+        "--num_workers",
+        type=int,
+        default=NUM_WORKERS,
+        help="Number of workers used in data loading",
+    )
+    parser.add_argument(
+        "--drop_last",
+        type=bool,
+        default=DROP_LAST,
+        help="Set dataset size to divisible number.",
+    )
+    parser.add_argument(
+        "--device", type=str, default=DEVICE, help="Set device used for training (GPU)"
+    )
+
+    return parser
+
+
+def main(
+    img_dir: Path,
+    train_filenames: Path,
+    file_format: str,
+    epochs: int,
+    learning_rate: float,
+    batch_size: int,
+    num_workers: int,
+    drop_last: bool,
+    device: str,
+):
     writer = SummaryWriter(r"./scratch/log/koala")
 
-    dataset = KoalaDataset(load_image_names(DEFAULT_TRAIN), DEFAULT_IMG_DIR, "{}.JPEG")
+    dataset = KoalaDataset(load_image_names(train_filenames), img_dir, file_format)
     dataloader = DataLoader(
         dataset,
-        batch_size=BATCH_SIZE,
-        num_workers=NUM_WORKERS,
+        batch_size=batch_size,
+        num_workers=num_workers,
         pin_memory=PIN_MEMORY,
-        drop_last=DROP_LAST,
+        drop_last=drop_last,
         persistent_workers=PERSISTENT_WORKERS,
         shuffle=SHUFFLE,
     )
 
     model = Colorization()
-    model.to(DEVICE)
+    model.to(device)
     criterion = nn.MSELoss()
     optimizer = optim.Adam(
-        filter(lambda a: a.requires_grad, model.parameters()), lr=LEARNING_RATE
+        filter(lambda a: a.requires_grad, model.parameters()), lr=learning_rate
     )
 
     running_loss = 0.0
-    print(f"Training on:\t {get_device_properties(DEVICE)}.")
+    print(f"Training on:\t {get_device_properties(device)}.")
     total_batches = 0
-    for epoch in trange(EPOCHS, unit="Epoch"):
+    for epoch in trange(epochs, unit="Epoch"):
         for batch_num, (inputs, targets) in tqdm(
             enumerate(dataloader),
             desc=f"Epoch {epoch}",
@@ -76,7 +139,7 @@ def main():
             total=len(dataset) // BATCH_SIZE,
             leave=False,
         ):
-            inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
+            inputs, targets = inputs.to(device), targets.to(device)
             predicted = model(inputs)
             loss = criterion(predicted, targets)
             optimizer.zero_grad()
@@ -101,4 +164,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = parse_args()
+    main(**vars(parser.parse_args()))
